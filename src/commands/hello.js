315c765f7076a1deb2aeb6c512b97b42336f25b2
alwaysneverr <83473354+alwaysneverr@users.noreply.github.com.>
2026-07-20 07:27:29 +0700

    add: custom hello message

--- src/commands/hello.js
+++ src/commands/hello.js
@@ -25,9 +25,13 @@ export default {
  
     const target = interaction.options.getUser('user') ?? interaction.user;
     const isSelf = target.id === interaction.user.id;
+    const isBot = target.id === interaction.client.user.id;
+
     const greeting = isSelf
-      ? `👋 Hey there, ${target}!`
-      : `👋 ${interaction.user} says hello to ${target}!`;
+      ? `Yo, Captain ${interaction.user}!`
+      : isBot 
+      ? `Tch, stop making me talk to myself.` 
+      : `Oi, Captain ${interaction.user} told me to say hello to you, ${target}. I don't know why he can't say it himself though. Whatevs.`;
  
     await interaction.editReply(greeting);
   },
