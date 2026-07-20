d44e6424f6de3474afcf308792ff8c83aed523b6
alwaysneverr <83473354+alwaysneverr@users.noreply.github.com.>
2026-07-20 23:22:23 +0700

    update: modify bot's online message and activity status

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,12 +1,20 @@
-import { Events } from 'discord.js';
+import { Events, ActivityType } from 'discord.js';
  
 export default {
   name: Events.ClientReady,
   once: true,
  
   execute(client) {
-    console.log(`\n🤖 Bot is online! Logged in as: ${client.user.tag}`);
-    console.log(`📡 Serving ${client.guilds.cache.size} server(s)\n`);
-    client.user.setActivity('with discord.js');
+    console.log(`\nAlright, I'm here. Oi, whaddaya mean who am I? It's ${client.user.username}! Quit joking around. Whaddaya need, Captain?`);
+    console.log(`📡 Yeah, yeah, I'm serving only ${client.guilds.cache.size} person in this world. You, Nine.\n`);
+    client.user.setActivity({
+      name: 'sigh',
+      type: ActivityType.Custom,
+      state: 'Alright then, whaddaya want?',
+      emoji: {
+        id: '1528770862461747251',
+        name: 'GhostWeary',
+      }
+    });
   },
 };
